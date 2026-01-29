85832e7b2b11f8aafc90fd997e186272199ba9b5
Miguel <nunezmiguel.tj@gmail.com>
2026-01-28 17:58:33 -0800

    Adding custom user model

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
+
+User = get_user_model()
 
 class LoginForm(forms.Form):
     email = forms.EmailField()
@@ -12,27 +13,34 @@ class LoginForm(forms.Form):
         password = cleaned_data.get("password")
 
         if email and password:
-            user = authenticate(username=email, password=password)
+            user = authenticate(email=email, password=password)
             if user is None:
                 raise forms.ValidationError("Invalid email or password")
-            self.user = user  # store for later use
+            if not user.is_active:
+                raise forms.ValidationError("This account is not active.")
+            cleaned_data["user"] = user
         return cleaned_data
 
 
-class RegisterForm(forms.Form):
-    # username = forms.CharField(max_length=25)
-    first_name = forms.CharField(max_length=50)
-    last_name = forms.CharField(max_length=50)
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
+class RegisterForm(forms.ModelForm):
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
 
-    def clean_email(self):
-        email = self.cleaned_data['email']
-        if User.objects.filter(email=email).exists():
-            raise forms.ValidationError("This email already exists.")
-        return email
-    # def clean_username(self):
-    #     username = self.cleaned_data['username']
-    #     if User.objects.filter(username=username).exists():
-    #         raise forms.ValidationError("This username already exists.")
-    #     return username
+    class Meta:
+        model = User
+        fields = ("email", "first_name", "last_name", "avatar")
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Password don't match.")
+        return password2
+
+    def save(self, should_commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if should_commit:
+            user.save()
+        return user
+
