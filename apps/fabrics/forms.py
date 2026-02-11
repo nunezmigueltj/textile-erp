933c25b3b5e9ca302551f77e24057af0874183c2
Miguel <nunezmiguel.tj@gmail.com>
2026-02-10 22:56:52 -0800

    Still working in the add fabric template

--- apps/fabrics/forms.py
+++ apps/fabrics/forms.py
@@ -4,4 +4,18 @@ from .models import Fabric
 class AddFabricForm(forms.ModelForm):
     class Meta:
         model = Fabric
-        fields = '__all__'
+        fields = ['item_code', 'vendor_code', 'vendor', 'fabric_class', 'content', 'weight', 'country_of_origin', 'open_or_tubular', 'yarn_type', 'yarn_size', 'cuttable']
+        widgets = {
+            'item_code': forms.TextInput(attrs={'class': 'form-control'}),
+            'vendor_code': forms.TextInput(attrs={'class': 'form-control'}),
+            'vendor': forms.Select(attrs={'class': 'form-select'}),
+            'fabric_class': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.TextInput(attrs={'class': 'form-control'}),
+            'weight': forms.TextInput(attrs={'class': 'form-control'}),
+            'country_of_origin': forms.TextInput(attrs={'class': 'form-control'}),
+            'open_or_tubular': forms.TextInput(attrs={'class': 'form-control'}),
+            'yarn_type': forms.TextInput(attrs={'class': 'form-control'}),
+            'yarn_size': forms.TextInput(attrs={'class': 'form-control'}),
+            'cuttable': forms.TextInput(attrs={'class': 'form-control'}),
+            # 'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'company@example.com'}),
+        }
--- apps/fabrics/views.py
+++ apps/fabrics/views.py
@@ -3,13 +3,46 @@ from .models import Fabric
 from .forms import AddFabricForm
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
+from django.db.models import Q
+from django.core.paginator import Paginator
+
 
 # Create your views here.
 @never_cache
 @login_required
 def fabrics_list(request):
-    fabrics = Fabric.objects.all()
-    return render(request, 'fabrics/home.html', {'fabrics': fabrics})
+    q = request.GET.get('q')
+    DEFAULT_ROWS_PER_PAGE = 25 # default to 25
+    rows_per_page = request.GET.get('rowsPerPage', DEFAULT_ROWS_PER_PAGE)
+    print(rows_per_page)
+
+    try:
+        rows_per_page = int(rows_per_page)
+    except ValueError:
+        rows_per_page = DEFAULT_ROWS_PER_PAGE
+
+
+    fabrics = Fabric.objects.all().order_by('id')
+
+    if q:
+        q=q.strip()
+        fabrics = Fabric.objects.filter(
+                Q(item_code__icontains=q) |
+                Q(vendor_code__icontains=q) |
+                Q(vendor__company__name__icontains=q) |
+                Q(fabric_class__icontains=q) |
+                Q(content__icontains=q) |
+                Q(country_of_origin__icontains=q)
+            ).order_by('id')
+
+        
+    paginator = Paginator(fabrics, rows_per_page)
+    page_number = request.GET.get('page')
+    print(f"page_number:::{page_number}")
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'fabrics/home.html', {'page_obj': page_obj, 'rows_per_page': rows_per_page})
+
 
 def add_fabric(request):
     if request.method == 'POST':
